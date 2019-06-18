70206ec6171f05629af9f7e89866299da23de4d6
zx <zhangxukim@gmail.com>
2019-06-19 01:03:16 +0800

    roe20

--- jobs/daily_job.py
+++ jobs/daily_job.py
@@ -148,7 +148,7 @@ def daily_defensive(tmp_datetime, res_data):
     logger.debug(data_def)
     res_data.defensive = data_def.to_html()
     # 由于buffett的ROE是10年连续20，牛逼的成长，所以买入放宽标准到65, 卖出标准放宽到110。 市盈率30是极限
-    data_buf = daily_common(cur_day, "ts_res_buffett", 110, 30, 0.02, 6.0)
+    data_buf = daily_common(cur_day, "ts_res_buffett", 110, 35, 0.02, 7.0)
     logger.debug(data_buf)
     res_data.buffett = data_buf.to_html()
 
